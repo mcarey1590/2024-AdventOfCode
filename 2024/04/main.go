866c2db6c1a3ebf5e95a24476efd26c25603eadb
mcareyafg <117487776+mcareyafg@users.noreply.github.com>
2024-12-07 20:50:57 -0600

    feat: day 4 1 and 2 solved

--- 2024/04/main.go
+++ 2024/04/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"AdventOfCode/utils"
 	"fmt"
 
 	"codeberg.org/derat/advent-of-code/lib"
@@ -23,14 +24,13 @@ var (
 )
 
 type LetterPos struct {
-	letter string
+	letter byte
 	x      int
 	y      int
 }
 
 type Word struct {
 	value string
-	shape [][]int // 1 = letter, 0 = don't care
 	path  []LetterPos
 }
 
@@ -41,54 +41,8 @@ type WordSearch struct {
 	h          int
 }
 
-func (ws WordSearch) checkDirection(dir Direction, word Word) *Word {
-	lastPath := word.path[len(word.path)-1]
-	x := lastPath.x + dir.x
-	y := lastPath.y + dir.y
-
-	if x < 0 || x >= ws.w || y < 0 || y >= ws.h {
-		return nil
-	}
-
-	lookingFor := word.value[len(word.path)]
-	if lookingFor != ws.grid[y][x] {
-		return nil
-	}
-
-	newWord := Word{value: word.value, path: append(word.path, LetterPos{letter: string(lookingFor), x: x, y: y})}
-
-	if len(newWord.path) == len(word.value) {
-		return &newWord
-	}
-
-	return ws.checkDirection(dir, newWord)
-
-}
-
-func (ws WordSearch) findWordStartingAt(x int, y int, word Word) []Word {
-	if ws.grid[y][x] != word.value[0] {
-		return []Word{}
-	}
-
-	var foundWords []Word
-	for _, dir := range ws.directions {
-		found := ws.checkDirection(dir, Word{value: word.value, path: []LetterPos{{letter: string(word.value[0]), x: x, y: y}}})
-		if found != nil {
-			fmt.Println("Found words: ", found)
-			foundWords = append(foundWords, *found)
-		}
-	}
-
-	return foundWords
-}
-
-func (ws WordSearch) findWordShapeStartingAt(x int, y int, word Word) []Word {
-
-}
-
 func main() {
-	grid := lib.NewByteGridString("MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX")
-	//grid := lib.InputByteGrid("2024/4")
+	grid := lib.InputByteGrid("2024/4")
 	solvePuzzle(grid)
 }
 
@@ -127,25 +81,130 @@ func part1(ws WordSearch) int {
 func part2(ws WordSearch) int {
 	var foundWords []Word
 	word := Word{
-		value: "XMAS",
-		shape: [][]int{
-			{1, 0, 1},
-			{0, 1, 0},
-			{1, 0, 1},
-		},
+		value: "MAS",
 	}
 
 	for y := 0; y < ws.h; y++ {
 		for x := 0; x < ws.w; x++ {
-			fmt.Println("Checking: ", x, y)
-			found := ws.findWordShapeStartingAt(x, y, word)
+			found := ws.findIntersectingWords(x, y, word)
 
-			if len(found) > 0 {
-				fmt.Println("Found words - base: ", found)
-				foundWords = append(foundWords, found...)
+			if found != nil {
+				foundWords = append(foundWords, *found)
 			}
 		}
 	}
 
 	return len(foundWords)
+}
+
+func (ws WordSearch) findWordStartingAt(x int, y int, word Word) []Word {
+	if ws.grid[y][x] != word.value[0] {
+		return []Word{}
+	}
+
+	var foundWords []Word
+	for _, dir := range ws.directions {
+		found := ws.checkDirection(dir, Word{value: word.value, path: []LetterPos{{letter: word.value[0], x: x, y: y}}})
+		if found != nil {
+			foundWords = append(foundWords, *found)
+		}
+	}
+
+	return foundWords
+}
+
+func (ws WordSearch) getNextLetter(currentPos LetterPos, dir Direction) *LetterPos {
+	x := currentPos.x + dir.x
+	y := currentPos.y + dir.y
+
+	if x < 0 || x >= ws.w || y < 0 || y >= ws.h {
+		return nil
+	}
+
+	return &LetterPos{letter: ws.grid[y][x], x: x, y: y}
+}
+
+func (ws WordSearch) checkDirection(dir Direction, word Word) *Word {
+	lastPath := word.path[len(word.path)-1]
+	nextLetter := ws.getNextLetter(lastPath, dir)
+
+	lookingFor := word.value[len(word.path)]
+	if nextLetter == nil || lookingFor != nextLetter.letter {
+		return nil
+	}
+
+	newWord := Word{value: word.value, path: append(word.path, *nextLetter)}
+
+	if len(newWord.path) == len(word.value) {
+		return &newWord
+	}
+
+	return ws.checkDirection(dir, newWord)
+
+}
+
+func (ws WordSearch) findIntersectingWords(x int, y int, word Word) *Word {
+	wordLength := len(word.value)
+	lib.Assertf(wordLength%2 == 1, "Word value must be an odd number of characters")
+	distanceFromCenter := wordLength / 2
+
+	centerLetter := LetterPos{letter: word.value[distanceFromCenter], x: x, y: y}
+
+	if ws.grid[y][x] != centerLetter.letter {
+		return nil
+	}
+
+	// Check diagonals
+	leftToRight := make([]LetterPos, wordLength)
+	leftToRight[distanceFromCenter] = centerLetter
+	rightToLeft := make([]LetterPos, wordLength)
+	rightToLeft[distanceFromCenter] = centerLetter
+
+	for i := 1; i <= distanceFromCenter; i++ {
+		left := distanceFromCenter - i
+		right := distanceFromCenter + i
+
+		if next := ws.getNextLetter(leftToRight[left+1], upLeft); next != nil {
+			leftToRight[left] = *next
+		}
+
+		if next := ws.getNextLetter(leftToRight[right-1], downRight); next != nil {
+			leftToRight[right] = *next
+		}
+
+		if next := ws.getNextLetter(rightToLeft[left+1], upRight); next != nil {
+			rightToLeft[left] = *next
+		}
+
+		if next := ws.getNextLetter(rightToLeft[right-1], downLeft); next != nil {
+			rightToLeft[right] = *next
+		}
+	}
+
+	return checkIntersectionWords(&word, leftToRight, rightToLeft)
+}
+
+func getWorkFromLetters(letters []LetterPos) string {
+	word := ""
+	for _, letter := range letters {
+		word += string(letter.letter)
+	}
+	return word
+}
+
+func checkIntersectionWords(word *Word, toRight []LetterPos, toLeft []LetterPos) *Word {
+	left := getWorkFromLetters(toLeft)
+	right := getWorkFromLetters(toRight)
+
+	if left != word.value && utils.Reverse(left) != word.value {
+		return nil
+	}
+
+	if right != word.value && utils.Reverse(right) != word.value {
+		return nil
+	}
+
+	word.path = append(toLeft, toRight...)
+
+	return word
 }
